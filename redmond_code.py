3ec6426b15e98087fd78f3c6b889acb5ba35b15b
tatesm <156850429+tatesm@users.noreply.github.com>
2024-03-28 17:27:55 -0600

    Add files via upload

---
+++ redmond_code.py
@@ -0,0 +1,90 @@
+import openmeteo_requests
+
+import requests_cache
+import pandas as pd
+from retry_requests import retry
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://archive-api.open-meteo.com/v1/archive"
+params = {
+	"latitude": 47.674,
+	"longitude": 122.1215,
+	"start_date": "2010-01-01",
+	"end_date": "2020-12-31",
+	"hourly": ["temperature_2m", "cloud_cover", "shortwave_radiation", "direct_normal_irradiance", "global_tilted_irradiance"],
+	"daily": ["weather_code", "temperature_2m_mean", "sunrise", "sunset", "daylight_duration", "sunshine_duration", "precipitation_sum", "rain_sum", "snowfall_sum", "shortwave_radiation_sum"],
+	"temperature_unit": "fahrenheit",
+	"wind_speed_unit": "mph",
+	"precipitation_unit": "inch",
+	"timezone": "America/Los_Angeles"
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation {response.Elevation()} m asl")
+print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+# Process hourly data. The order of variables needs to be the same as requested.
+hourly = response.Hourly()
+hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+hourly_cloud_cover = hourly.Variables(1).ValuesAsNumpy()
+hourly_shortwave_radiation = hourly.Variables(2).ValuesAsNumpy()
+hourly_direct_normal_irradiance = hourly.Variables(3).ValuesAsNumpy()
+hourly_global_tilted_irradiance = hourly.Variables(4).ValuesAsNumpy()
+
+hourly_data = {"date": pd.date_range(
+	start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+	end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = hourly.Interval()),
+	inclusive = "left"
+)}
+hourly_data["temperature_2m"] = hourly_temperature_2m
+hourly_data["cloud_cover"] = hourly_cloud_cover
+hourly_data["shortwave_radiation"] = hourly_shortwave_radiation
+hourly_data["direct_normal_irradiance"] = hourly_direct_normal_irradiance
+hourly_data["global_tilted_irradiance"] = hourly_global_tilted_irradiance
+
+hourly_dataframe_redmond = pd.DataFrame(data = hourly_data)
+print(hourly_dataframe_redmond)
+
+# Process daily data. The order of variables needs to be the same as requested.
+daily = response.Daily()
+daily_weather_code = daily.Variables(0).ValuesAsNumpy()
+daily_temperature_2m_mean = daily.Variables(1).ValuesAsNumpy()
+daily_sunrise = daily.Variables(2).ValuesAsNumpy()
+daily_sunset = daily.Variables(3).ValuesAsNumpy()
+daily_daylight_duration = daily.Variables(4).ValuesAsNumpy()
+daily_sunshine_duration = daily.Variables(5).ValuesAsNumpy()
+daily_precipitation_sum = daily.Variables(6).ValuesAsNumpy()
+daily_rain_sum = daily.Variables(7).ValuesAsNumpy()
+daily_snowfall_sum = daily.Variables(8).ValuesAsNumpy()
+daily_shortwave_radiation_sum = daily.Variables(9).ValuesAsNumpy()
+
+daily_data = {"date": pd.date_range(
+	start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+	end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = daily.Interval()),
+	inclusive = "left"
+)}
+daily_data["weather_code"] = daily_weather_code
+daily_data["temperature_2m_mean"] = daily_temperature_2m_mean
+daily_data["sunrise"] = daily_sunrise
+daily_data["sunset"] = daily_sunset
+daily_data["daylight_duration"] = daily_daylight_duration
+daily_data["sunshine_duration"] = daily_sunshine_duration
+daily_data["precipitation_sum"] = daily_precipitation_sum
+daily_data["rain_sum"] = daily_rain_sum
+daily_data["snowfall_sum"] = daily_snowfall_sum
+daily_data["shortwave_radiation_sum"] = daily_shortwave_radiation_sum
+
+daily_dataframe_redmond = pd.DataFrame(data = daily_data)
+print(daily_dataframe_redmond)
